fb63aca20f9a68864b988db10f91bc0f8c926793
KncskBenedek <koncsikbenedek@gmail.com>
2022-05-20 12:22:31 +0200

    tesztLoves nem működik, assert nem működik

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -12,10 +12,13 @@ package main;
 public class TorpedoTeszt {
     
     public static void main(String[] args) {
-        
+        tesztLoves(4);
     }
     
     public String tesztLoves(int poz){
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(4);
+        assert t.equals("találat") : "nem jó a találat ellenőrzése";
         return "";
     }
     
