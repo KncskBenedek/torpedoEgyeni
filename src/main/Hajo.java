b8772f805871759a735331acd13edf42e1242ff8
KncskBenedek <koncsikbenedek@gmail.com>
2022-05-20 12:40:34 +0200

    talalat() metódus helyesen működik

--- src/main/Hajo.java
+++ src/main/Hajo.java
@@ -3,8 +3,24 @@ package main;
 
 public class Hajo {
     private int[] pozicio;
+
+    public Hajo(int[] pozicio) {
+        this.pozicio = new int[3];
+        this.pozicio = pozicio;
+    }
     
+        
     public String talalat(int poz){
-        return "";
+        int i = 0;
+        while (i < pozicio.length && !(poz == pozicio[i])) {
+            i++;
+        }
+        String szoveg = "";
+        if (i < pozicio.length) {
+            szoveg = "találat";
+        }else{
+            szoveg = "nem talált";
+        }
+        return  szoveg;
     }
 }
--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -12,14 +12,21 @@ package main;
 public class TorpedoTeszt {
     
     public static void main(String[] args) {
-        tesztLoves(4);
+        
+       tesztLoves(4);
     }
     
-    public String tesztLoves(int poz){
-        Hajo hajo = new Hajo();
-        String t = hajo.talalat(4);
-        assert t.equals("találat") : "nem jó a találat ellenőrzése";
+    public static String tesztLoves(int poz){
+        int[] tomb = new int[3];
+        tomb[0] = 2;
+        tomb[1] = 3;
+        tomb[2] = 4;
+        Hajo hajo = new Hajo(tomb);
+        String t = hajo.talalat(poz);
+        assert t.equals("találat") || t.equals("nem találat")  : "nem jó a találat ellenőrzése";
         return "";
     }
+
+    
     
 }
